c47728a493049917acb734aa3da177473023a7bd
Ryan Brown <ryan393brown@gmail.com>
2021-04-24 17:49:32 -0700

    remove xgmaes and danny call out, fix scene

--- src/Scene.js
+++ src/Scene.js
@@ -9,31 +9,6 @@ import { a } from '@react-spring/three'
 // but it can also handle 3rd–party objs, just wrap them in "a".
 const AnimatedMaterial = a(MeshDistortMaterial)
 
-function Scene() {
-
-  // textures from the imported image
-  const texture = useLoader(THREE.TextureLoader, img)
-  const texture1 = useLoader(THREE.TextureLoader, img1)
-  const texture2 = useLoader(THREE.TextureLoader, img2)
-  const group = useRef();
-  // loading the table.gtlf file being imported into the component.
-  const { nodes } = useLoader(OBJLoader, table);
-  // useFrame will run outside of react in animation frames to optimize updates.
-  useFrame(() => {
-    group.current.rotation.x = 5.09;
-  });
-  return (
-    // Add a ref to the group. This gives us a hook to manipulate the properties of this geometry in the useFrame callback.
-    <group ref={group} position={[-12, -20, -10]} >
-      <mesh visible geometry={nodes.mesh_1.geometry}>
-      <meshPhongMaterial attach="material" color="gold" map={texture} map={texture1} map={texture2}/>
-      </mesh>
-      <mesh visible geometry={nodes.mesh_0.geometry}>
-      <meshPhongMaterial attach="material" color="#795C34" map={texture} map={texture1} map={texture2}/>
-      </mesh>
-    </group>
-  );
-}
 export default function Scene({ setBg }) {
   const sphere = useRef()
   const light = useRef()
