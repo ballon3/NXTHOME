cca946c728f536b26f5dd6ccf5541f93b0b13f47
Ryan Brown <ryan393brown@gmail.com>
2021-04-29 02:34:30 -0700

    home update w deck, prt 2

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default function App() {
         <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
       </Canvas>
       <Overlay fill={fill} />
-    </a.main>
+
+      </a.main>
   )
 }
